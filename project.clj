7c12f432f8c7d717af465b3b41be721f05a5c66b
James Conroy-Finn <james@logi.cl>
2015-05-13 16:22:48 +0100

    Remove examples

--- project.clj
+++ project.clj
@@ -22,11 +22,7 @@
   :profiles {:dev {:dependencies [[ring/ring-jetty-adapter "1.2.1" :exclusions [joda-time]]
                                   [ring-mock "0.1.2"]
                                   [ring/ring-devel "1.2.1" :exclusions [joda-time]]
-                                  [midje "1.6.0" :exclusions [org.clojure/clojure]]
-                                  ;; only for examples
-                                  [compojure "1.0.2" :exclusions [org.clojure/tools.macro]]
-                                  [org.clojure/clojurescript "0.0-1450"]]
-                   :source-paths [ "src" "examples/clj"]}
+                                  [midje "1.6.0" :exclusions [org.clojure/clojure]]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
@@ -34,8 +30,4 @@
   :source-paths ["src"]
   :test-paths ["test"]
 
-  :ring {:handler examples.server/handler
-         :adapter {:port 8000}}
-
-  :aliases {"examples" ["run" "-m" "examples.server"]
-            "test-all" ["with-profile" "+1.4:+1.5:+1.6" "test"]})
+  :aliases {"test-all" ["with-profile" "+1.4:+1.5:+1.6" "test"]})
